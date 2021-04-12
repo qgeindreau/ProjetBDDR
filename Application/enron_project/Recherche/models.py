af0b58bd10f92cdc211d3f02babc6f9e4a4e77f8
qgeindreau <geindreauquentin@hotmail.fr>
2021-04-12 14:01:50 +0200

    Ok

--- Application/enron_project/Recherche/models.py
+++ Application/enron_project/Recherche/models.py
@@ -1,6 +1,52 @@
 from django.db import models
 
 # Create your models here.
-class Users(models.Model)
-    name = models.CharField(max_lenght=200,unique=True)
-    
+
+class User(models.Model):
+    categorie = models.CharField(20)
+    nom =  models.CharField(20)
+    prenom = models.CharField(20)
+      
+    def __str__(self):
+        return 'Nom+: '+self.nom+' Prenom: '+self.prenom +' Categorie: ' +self.categorie
+
+class User_Email(models.Model):
+    user_id = models.ForeignKey(
+          User,
+          on_delete=models.CASCADE,
+          )
+    adr_Mail = models.EmailField(30)
+
+    def __str__(self):
+        return 'Addresse: '+self.adr_Mail
+
+class Mail(models.Model):
+    date = models.DateTimeField()
+    objet = models.CharField(30)
+    body = models.TextField()
+    is_a_response = models.BooleanField()
+    mail_user_id = models.ForeignKey(
+          User_Email,
+          on_delete=models.CASCADE,
+          ) #Id de celui qui envoie
+    def __str__(self):
+        return "Objet: "+ self.objet
+
+
+class Mail_Receiver_in(models.Model):
+    mail_id = models.ForeignKey(
+        Mail,
+        on_delete=models.CASCADE,
+        )
+    user_mail_id = models.ForeignKey(
+        User_Email,
+        on_delete=models.CASCADE,
+        ) #Id de celui qu recoit
+        def __str__(self):
+            return "Mail:"+self.mail_id+'Add:'+self.user_mail_id
+class Mail_Receiver_in(models.Model):
+    mail_id = models.ForeignKey(
+        Mail,
+        on_delete=models.CASCADE,
+        )
+    email=models.EmailField()
--- Utile/parsemail.py
+++ Utile/parsemail.py
@@ -1,10 +1,21 @@
-from email.parser import Parser
+from email.parser import Parser 
+import datetime
 with open('Utile/2.txt', "r") as f:
     data = f.read()
     
 email = Parser().parsestr(data)
 
-print("\nTo: " , email['to'])
-print("\n From: " , email['from'])
- 
-print("\n Subject: " , email['bcc'])
+date_time_str = email['date']
+#date_time_obj = datetime.datetime.strptime(date_time_str, "%a, %d %B, %Y")
+
+
+import maya
+
+dt = maya.parse(date_time_str).datetime()
+print(dt)
+import maya
+import datetime
+def conv_time(txt):
+    return maya.parse(txt).datetime()
+
+print(conv_time(date_time_str))
